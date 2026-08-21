c07c12b07e6c33b95e6a4161780e5f789da3ae12
harry-elearn <92331146+harry-elearn@users.noreply.github.com>
2026-08-22 00:17:42 +0530

    Update Basics_Program_01.py
    Added Print and format specifiers

--- Basics_Program_01.py
+++ Basics_Program_01.py
@@ -1,4 +1,7 @@
-#Identifiers # Keywords #DeclaringVariables #DynamicTyping
+#Identifiers 
+#Keywords 
+#DeclaringVariables 
+#DynamicTyping
 num = 12
 print(num,type(num))
 num = "A"
@@ -10,3 +13,26 @@ input_var = input("please enter data as per your wish")
 print(input_var)
 
 #adding from the Github edit option from web
+
+
+#print()
+#Format specifiers
+
+p = "India"
+q = 101.101
+r = 501
+print(p,q,r)
+print(p,q,r,sep=":")
+print(p,q,r,end=" ")
+print(p,q,r)
+print("q=%0.2f"%q)
+print("q=%1.1f"%q)
+print("q=%7.1f"%q)
+print("r=%8d" %r)
+print("r=%-8d" %r)
+print("r=%4d"%r)
+print("Value of p is %s" %p)
+print("Value of p is %%s", p)
+print("Value of p is %s" %p)
+
+
