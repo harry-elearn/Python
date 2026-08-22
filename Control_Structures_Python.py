dd3a7ab1dc22dcd5e67a76db384def5add280f19
harry-elearn <92331146+harry-elearn@users.noreply.github.com>
2026-08-22 23:05:23 +0530

    Update Control_Structures_Python.py
    nested if

--- Control_Structures_Python.py
+++ Control_Structures_Python.py
@@ -8,7 +8,7 @@ if (a>0):
 #Indentation in python
 
 #if 1==1:
-#print("Indentaion is Important")
+#print("Indentation is Important")
 
 #above fails due to - IndentationError: expected an indented block after 'if' statement on line 10
 
@@ -18,3 +18,29 @@ if a > 0 :
     print("Positive Integer")
 else:
     print("not positive Integer")
+
+#Multi-Way select statement
+
+a = 0
+if (a>0):
+    print("+ve")
+elif (a<0):
+    print("-ve")
+else:
+    print("its 0") 
+
+
+#nested if
+p = 40
+q = 20
+r = 5
+if (p>q):
+    if (p>r):
+        print("p is greatest")
+    else:
+        print("r is greatest")
+elif (q>r):
+    print("q is greatest")
+else :
+    print("r is greatest")
+
