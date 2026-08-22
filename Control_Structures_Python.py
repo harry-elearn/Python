5afc562e66c986334108834ee6c99cbeba1f6832
harry-elearn <92331146+harry-elearn@users.noreply.github.com>
2026-08-22 22:46:15 +0530

    Create Control_Structures_Python.py
    if else

---
+++ Control_Structures_Python.py
@@ -0,0 +1,20 @@
+#One-Way select statement
+
+a = 5
+if (a>0):
+    print("+ve integer")
+
+
+#Indentation in python
+
+#if 1==1:
+#print("Indentaion is Important")
+
+#above fails due to - IndentationError: expected an indented block after 'if' statement on line 10
+
+#Two-Way select statement
+a = -0
+if a > 0 :
+    print("Positive Integer")
+else:
+    print("not positive Integer")
