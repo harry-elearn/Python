8fc4794cf7e90debfc3a37c297a594fbcbf48290
harry-elearn <92331146+harry-elearn@users.noreply.github.com>
2026-08-26 00:19:16 +0530

    Update Control_Structures_Python.py
    Break, Continue, Pass

--- Control_Structures_Python.py
+++ Control_Structures_Python.py
@@ -215,3 +215,23 @@ total = sum(map(int, inp))
 print("Total is:", total)
 
 """
+
+
+#Break
+#Continue
+#Pass
+
+for i in range(1,5):
+    if i == 3 :
+        break
+    print(i)
+
+for i in range(1,5):
+    if i == 3 :
+        continue
+    print(i)
+
+for i in range(1,5):
+    if i == 3 :
+        pass
+    print(i)
